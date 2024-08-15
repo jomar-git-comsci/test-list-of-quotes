b8db6ab3e3a80ba918600fa2b1c593a22cb56f6b
Mona Lisa <jessalynlalatasantos07@gmail.com>
2024-08-16 07:19:46 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,27 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
+
+  List<String> quotes = [
+    'You better lose yourself in music the moment you own it never let it go',
+    'The way to get started is to quit talking and begin doing.',
+    'Spread love everywhere you go. Let no one ever come to you without leaving happier',
+    
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text("Awesome Quotes"),
+        centerTitle: true,
+        backgroundColor: Colors.lime,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote),
+        ).toList(),
+      ),
+    );
   }
 }
